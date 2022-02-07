6174dad28997e8b38e9949ed022829e6cc790734
subramanian1503 <roopan1503@gmail.com>
2022-02-07 08:47:40 +0530

    [card-77273183] Update copyrights
    Updated copyrights.

--- src/test/java/com/upskillyourself/list/ListApplicationTests.java
+++ src/test/java/com/upskillyourself/list/ListApplicationTests.java
@@ -1,3 +1,20 @@
+/*
+ * *
+ *  * *******************************************************************************
+ *  * Copyright ©2022.
+ *   Upskillyourself - All rights reserved.
+ *  *
+ *  * All information contained herein is, and remains the property of Upskillyourself.
+ *  * Upskillyourself including, without limitation, all software and other elements thereof,
+ *  * are owned or controlled exclusively by Upskillyourself and protected by copyright, patent
+ *  * and other laws. Use without permission is prohibited.
+ *  * Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *
+ *  * For further information contact Upskillyourself at info@Upskillyourself.com.
+ *  * *******************************************************************************
+ *
+ */
+
 package com.upskillyourself.list;
 
 import org.junit.jupiter.api.Test;
@@ -6,8 +23,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ListApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
